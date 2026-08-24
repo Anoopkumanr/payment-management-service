4166fc9b6b585f98aeaa31eb7c9382f38d6fdf98
Anoopkumanr <32436383+Anoopkumanr@users.noreply.github.com>
2026-08-25 00:04:42 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,7 +16,7 @@ pipeline {
         stage('Checkout') {
             steps {
                 git branch: 'main',
-                    url: 'https://github.com/Anoopkumanr/payment-management-service-api.git'
+                    url: 'https://github.com/Anoopkumanr/payment-management-service.git'
             }
         }
 
