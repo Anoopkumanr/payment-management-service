363c843b5c76cb0e54b310a6e41dfcd53ee23af8
Anoopkumanr <32436383+Anoopkumanr@users.noreply.github.com>
2026-08-25 00:24:48 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,7 +7,7 @@ pipeline {
     }
 
     environment {
-        DOCKER_IMAGE = "YOUR_DOCKERHUB_USERNAME/springboot-app"
+        DOCKER_IMAGE = "payment-management-service"
         DOCKER_TAG   = "${env.BUILD_NUMBER}"
     }
 
